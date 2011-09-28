39b7599accae3031689dd98e1b2f209ae08ebb7b
rtoal <rtoal@lmu.edu>
2011-09-27 18:33:18 -0700

    Edited chapter07/colored-circle-datatype.js via GitHub

--- chapter07/colored-circle-datatype.js
+++ chapter07/colored-circle-datatype.js
@@ -1,10 +1,10 @@
 /*
- * A colored circle datatype, a subtype of Circle.  Synopsis:
+ * A colored circle data type, a subtype of Circle.  Synopsis:
  *
  * var c = new ColoredCircle(5, {red: 0.2, green: 0.8, blue: 0.33});
  * c.radius => 5
  * c.area() => 25pi
- * c.perimiter() => 10pi
+ * c.perimeter() => 10pi
  * c.brighten(1.1) changes color to {red: .22, green: .88, blue: .363}
  */
 var ColoredCircle =  function (radius, color) {
