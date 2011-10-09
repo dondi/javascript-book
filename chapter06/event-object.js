73328c292f34a0098cc9d59b8bce978606093666
dondi <dondi@lmu.edu>
2011-10-08 19:30:40 -0700

    Fixed misnamed event object.

--- chapter06/event-object.js
+++ chapter06/event-object.js
@@ -1,8 +1,11 @@
 document.getElementById("wonder").onclick = function (e) {
-    if (!e) e = event;
+    if (!e) {
+        e = event;
+    }
+
     var status = document.getElementById("status");
     var selection = this.options[this.selectedIndex].text;
-    status.innerHTML = event.shiftKey ? selection.toUpperCase() :
-        event.altKey ? selection.toLowerCase() :
+    status.innerHTML = e.shiftKey ? selection.toUpperCase() :
+        e.altKey ? selection.toLowerCase() :
         selection;
 };
