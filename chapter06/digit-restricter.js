95ddf803825b6e25d1b09e0e16cfa301ada8a1c6
dondi <dondi@lmu.edu>
2012-01-06 15:09:25 -0800

    Made digit-restricter.js pass JSLint (with regex consideration).

--- chapter06/digit-restricter.js
+++ chapter06/digit-restricter.js
@@ -2,41 +2,44 @@
  * Eliminates all nondigits from the given string.
  */
 var restrictToDigits = function (string) {
-    return string.replace(/[^\d]/g, "");
-};
+        return string.replace(/[^\d]/g, "");
+    },
 
-/*
- * Checks if the given value is all digits.
- */
-var isAllDigits = function (string) {
-    return string.match(/^\d*$/);
-};
+    /*
+     * Checks if the given value is all digits.
+     */
+    isAllDigits = function (string) {
+        return string.match(/^\d*$/);
+    },
 
-/*
- * Rejects nondigit keypresses.
- */
-var handleInputKeyPress = function (e) {
-    if (!e) e = event;
-    if (!isAllDigits(String.fromCharCode(e.charCode))) {
-        // Make sure keypress doesn't cause a character to appear
-        if (e.preventDefault) {
-            e.preventDefault();
-        } else {
-            e.returnValue = false;
+    /*
+     * Rejects nondigit keypresses.
+     */
+    handleInputKeyPress = function (e) {
+        if (!e) {
+            e = event;
         }
-    }
-};
 
-/*
- * Rejects nondigits in this.value, and reselects the text field.
- */
-var handleInputBlur = function () {
-    if (!isAllDigits(this.value)) {
-        alert("Sorry, only 0-9 are allowed here.");
-        this.value = restrictToDigits(this.value);
-        this.select();
-    }
-};
+        if (!isAllDigits(String.fromCharCode(e.charCode))) {
+            // Make sure keypress doesn't cause a character to appear
+            if (e.preventDefault) {
+                e.preventDefault();
+            } else {
+                e.returnValue = false;
+            }
+        }
+    },
+
+    /*
+     * Rejects nondigits in this.value, and reselects the text field.
+     */
+    handleInputBlur = function () {
+        if (!isAllDigits(this.value)) {
+            alert("Sorry, only 0-9 are allowed here.");
+            this.value = restrictToDigits(this.value);
+            this.select();
+        }
+    };
 
 document.getElementById("input1").onkeypress = handleInputKeyPress;
 document.getElementById("input2").onblur = handleInputBlur;
